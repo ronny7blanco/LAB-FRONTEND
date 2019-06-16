cbdb01ab0ca6d5c5eeda627b9e5eafca2ff11df9
ronny7blanco <ronnyblanco19@gmail.com>
2019-06-15 23:03:42 -0600

    Primera Guía

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticulosService }  from './services/articulos.service';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { LoginComponent } from './login/login.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagefoundComponent,
+    LoginComponent,
+    PagesComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ArticulosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
